f1c2944120f72fe91750c31a5a1d7d9f686e1abe
PaawanBhatt118 <paawanbhatt39@gmail.com>
2021-04-07 14:55:10 +0530

    first commit

---
+++ utils/build/webpack.config.js
@@ -0,0 +1,13 @@
+// var path = require('path');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+// module.exports = {
+//   mode: 'development',
+//   entry: 'src/Three.js',
+//   output: {
+//     path: path.resolve(__dirname, 'editor'),
+//     filename: 'index.html',
+//   },
+// };
+
